147547b7663ab9d072a8f440922f3812372025a0
Asha Schwegler <schweglerasha@gmail.com>
2022-06-02 07:55:06 +0200

    added proxy pattern

---
+++ src/Java/Observer/Observer.java
@@ -0,0 +1,5 @@
+package Java.Observer;
+
+public interface Observer {
+    public void update(Object obj, Object arg);
+}
--- src/Java/Proxy/ProxyInternetAccess.java
+++ src/Java/Proxy/ProxyInternetAccess.java
@@ -1,2 +1,24 @@
-package Java.Proxy;public class ProxyInternetAccess {
+package Java.Proxy;
+
+public class ProxyInternetAccess implements OfficeIntrnetAccess{
+    private String employeeName;
+    private RealInternetAccess  realaccess;
+
+    public ProxyInternetAccess(String employeeName) {
+        this.employeeName = employeeName;
+    }
+
+    @Override
+    public void grantInternetAccess() {
+        if(getRole(employeeName)>4){
+            realaccess = new RealInternetAccess(employeeName);
+            realaccess.grantInternetAccess();
+        }else{
+            System.out.println("No Internet access granted. Your job level is below 5");
+        }
+    }
+
+    public int getRole(String employeeName){
+        return 9;
+    }
 }
